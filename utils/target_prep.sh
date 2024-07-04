3b4bbbfc0caf925a4b323be85ae9518a62f9e872
Chunt0 <chunt.mail@gmail.com>
2024-07-03 22:31:02 -0700

    2024-07-03 22:31:02

--- utils/target_prep.sh
+++ utils/target_prep.sh
@@ -2,29 +2,19 @@
 # This script should be used to format images for a fresh labeling run.
 # This will remove any existing txt files in this folder.
 
-folder_path="../assets/link-5"
+folder_path="../assets/logo"
 
 # Navigate to the folder containing the images
 cd "$folder_path" || exit
-
-# Remove any existing text files
-rm *.txt
 
 # Initialize counter
 count=1
 
 # Loop through each image file in the folder
-for file in *.{webp,WEBP,jpg,JPG,jpeg,JPEG,png,PNG}; do
+for file in *.{png,PNG,svg,SVG}; do
     # Check if the file is a regular file
     if [ -f "$file" ]; then
-        # Convert the webp images to PNG if applicable
-        if [[ "$file" == *.{webp,WEBP} ]]; then
-            convert "$file" "${file%.*}.png"
-            rm "$file"
-        fi
-        # Rename the image file to .png format with sequential numbering
-        mv "$file" "people-$count.png"
-        
+        mv "$file" "$count.svg"
         # Increment counter
         ((count++))
     fi
