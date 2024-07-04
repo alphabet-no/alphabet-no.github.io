509deea486086a8242ffa5d61884597e885f86bb
Chunt0 <chunt.mail@gmail.com>
2024-07-03 23:06:39 -0700

    2024-07-03 23:06:39

--- utils/target_prep.sh
+++ utils/target_prep.sh
@@ -2,19 +2,16 @@
 # This script should be used to format images for a fresh labeling run.
 # This will remove any existing txt files in this folder.
 
-folder_path="../assets/logo/pngs"
-
-# Navigate to the folder containing the images
-cd "$folder_path" || exit
+DIRECTORY=$1
 
 # Initialize counter
 count=1
 
 # Loop through each image file in the folder
-for file in *.{png,PNG,svg,SVG}; do
+for file in "$DIRECTORY"/*.{png,PNG,svg,SVG}; do
     # Check if the file is a regular file
     if [ -f "$file" ]; then
-        mv "$file" "$count.png"
+        mv "$file" "$DIRECTORY"/"$count.png"
         # Increment counter
         ((count++))
     fi
