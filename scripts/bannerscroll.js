ac6d4eeb916d14fe7c22baec039c9a655a0ca4db
Chunt0 <chunt.mail@gmail.com>
2024-07-04 16:18:58 -0700

    2024-07-04 16:18:58

--- scripts/bannerscroll.js
+++ scripts/bannerscroll.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
     const bannerContent = document.getElementById('banner-content');
-    const numberOfImages = 252; // Total number of images
+    const numberOfImages = 253; // Total number of images
     const imageUrlPrefix = "./assets/logo/"; // Prefix of your image URLs
     const imageUrlSuffix = ".png"; // Suffix of your image URLs
     
-    for (let i = 0; i <= numberOfImages; i++) {
+    for (let i = 1; i <= numberOfImages; i++) {
         const img = document.createElement('img');
         img.src = `${imageUrlPrefix}${i}${imageUrlSuffix}`;
         img.alt = `Logo ${i}`;
