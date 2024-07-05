cdd88693580c6d7a0217aa1e3a5bd3b072287bfc
Chunt0 <chunt.mail@gmail.com>
2024-07-05 12:31:39 -0700

    2024-07-05 12:31:39

--- utils/text_prep.py
+++ utils/text_prep.py
@@ -1,13 +1,13 @@
 import os
 import random
 
-path = "../assets/text-armyofuganda-orig.txt"
+path = "../assets/final-text.txt"
 
 with open(path, 'r') as f:
     lines = [line.strip() for line in f.readlines()]
 
 random.shuffle(lines)
 
-with open("../assets/text-armyofuganda.txt", "w") as f:
+with open("../assets/final-text-formatted.txt", "w") as f:
     for line in lines:
         f.write(f'\t"{line}",\n')
